d61b824a4d447547421aa4889beafd1dcfcf6a3e
toyamahiroyuki <toyama.2523@gmail.com>
2020-02-24 12:29:06 +0000

    finish[応用課題4]

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,39 @@
+class RelationshipsController < ApplicationController
+
+def create
+    user = User.find(params[:follow_id])
+    following = current_user.follow(user)
+    if following.save
+      flash[:success] = 'ユーザーをフォローしました'
+      redirect_back(fallback_location: root_path)
+    else
+      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def destroy
+    user = User.find(params[:follow_id])
+    following = current_user.unfollow(user)
+    if following.destroy
+      flash[:success] = 'ユーザーのフォローを解除しました'
+      redirect_back(fallback_location: root_path)
+    else
+      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def follow
+  	@user =User.find(params[:user_id])
+  	@users = @user.followings
+  	# render 'show_follow'
+  end
+
+  def follower
+  	@user =User.find(params[:user_id])
+  	@users = @user.followers
+  	# render 'show_follow'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+  	get "follow" => "relationships#follow"
+  	get "follower" => "relationships#follower"
+  end
+  resources :relationships,only: [:create, :destroy]
   resources :books do
      resource :book_comment ,only: [:create, :destroy]
      resource :favorite ,only: [:create, :destroy]
